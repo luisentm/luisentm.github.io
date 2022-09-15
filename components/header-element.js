9086925b6d7167abcb6443c01c30421a6fd265db
luisentm <luisenrriquetm@gmail.com>
2022-09-15 12:30:07 -0400

    Update header-element.js

--- components/header-element.js
+++ components/header-element.js
@@ -1,15 +1,12 @@
-
-<script>
-	import {defineComponent} from 'vue';
-	export default defineComponent({
-		props:[],
-		setup(){
-			const count = ref(0)
-			return { count }
-		},
-		template:`
-		<template>
-			<button @click="count++">You clicked me {{ count }} times.</button>
-		</template>`
-	});
-</script>
+import {defineComponent} from 'vue';
+export default defineComponent({
+	props:[],
+	setup(){
+		const count = ref(0)
+		return { count }
+	},
+	template:`
+	<template>
+		<button @click="count++">You clicked me {{ count }} times.</button>
+	</template>`
+});
