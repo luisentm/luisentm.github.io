1e8608c31f3ebd088108f1f9e1ecd81eea643c76
luisentm <luisenrriquetm@gmail.com>
2024-07-03 14:41:02 -0400

    f. vue

--- app.js
+++
@@ -1,6 +0,0 @@
-import {defineCustomElement} from 'vue';
-import * as header from './components/header-element.js';
-
-const header_element = defineCustomElement(header);
-
-customElements.define('header-element',header_element);
